448a8b9693a4915280bd5f99206c890cde24269f
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-19 18:55:47 -0300

    tabela de classificacao

--- tabelaclassificacao01/script.js
+++ tabelaclassificacao01/script.js
@@ -1,3 +1,19 @@
+var paulo = {
+  nome:'Paulo', 
+  vitoria: 0,
+  empate: 0,
+  derrota: 0,
+  pontos: 0
+}
+var Rafa = {
+  nome: "Rafa",
+  vitoria: 0,
+  empate: 0,
+  derrota: 0,
+  pontos: 0,
+}
+
+
 function adicionarVitoria() {}
 function adicionarEmpate() {}
 function adicionarDerrota() {}
