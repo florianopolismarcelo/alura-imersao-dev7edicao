75af06eec881324d8b9393c26a9d6acee9e2c754
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-19 19:42:35 -0300

    tabela de classificacao

--- tabelaclassificacao01/script.js
+++ tabelaclassificacao01/script.js
@@ -1,18 +1,45 @@
-var paulo = {
-  nome:'Paulo', 
+var marcelo = {
+  nome:'Marcelo', 
   vitoria: 0,
   empate: 0,
   derrota: 0,
   pontos: 0
 }
-var Rafa = {
-  nome: "Rafa",
+var Alexandre = {
+  nome: "Alexandre",
+  vitoria: 0,
+  empate: 0,
+  derrota: 0,
+  pontos: 0,
+}
+var Souza = {
+  nome: "Souza",
+  vitoria: 0,
+  empate: 0,
+  derrota: 0,
+  pontos: 0,
+}
+var Motta = {
+  nome: "Motta",
   vitoria: 0,
   empate: 0,
   derrota: 0,
   pontos: 0,
 }
 
+var elementoTabela = document.getElementById('tabelaJogadores')
+elementoTabela.innerHTML = `
+<tr>
+              <td>${marcelo.nome}</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><button onClick="adicionarVitoria()">Vitória</button></td>
+              <td><button onClick="adicionarEmpate()">Empate</button></td>
+              <td><button onClick="adicionarDerrota()">Derrota</button></td>
+            </tr>
+`
 
 function adicionarVitoria() {}
 function adicionarEmpate() {}
