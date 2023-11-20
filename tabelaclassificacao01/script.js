f3263bc50d309e269973534f1e49d6a3f8623900
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-11-20 10:42:09 -0300

    tabela de classificacao

--- tabelaclassificacao01/script.js
+++ tabelaclassificacao01/script.js
@@ -1,9 +1,9 @@
 var marcelo = {
-  nome:'Marcelo', 
+  nome: "Marcelo",
   vitoria: 0,
   empate: 0,
   derrota: 0,
-  pontos: 0
+  pontos: 0,
 }
 var Alexandre = {
   nome: "Alexandre",
@@ -27,12 +27,12 @@ var Motta = {
   pontos: 0,
 }
 
-var elementoTabela = document.getElementById('tabelaJogadores')
+var elementoTabela = document.getElementById("tabelaJogadores")
 
 exibirNaTela()
 
 function exibirNaTela() {
-elementoTabela.innerHTML = `
+  elementoTabela.innerHTML = `
 <tr>
               <td>${marcelo.nome}</td>
               <td>${marcelo.vitoria}</td>
@@ -40,24 +40,27 @@ elementoTabela.innerHTML = `
               <td>${marcelo.derrota}</td>
               <td>${marcelo.pontos}</td>
               <td><button onClick="adicionarVitoria(marcelo)">Vitória</button></td>
-              <td><button onClick="adicionarEmpate()">Empate</button></td>
-              <td><button onClick="adicionarDerrota()">Derrota</button></td>
+              <td><button onClick="adicionarEmpate(marcelo)">Empate</button></td>
+              <td><button onClick="adicionarDerrota(marcelo)">Derrota</button></td>
             </tr>
 `
 }
 
 function adicionarVitoria(jogador) {
-  jogador.vitoria++;
-  jogador.pontos = jogador.pontos + 3;
-  exibirNaTela()
+  jogador.vitoria++
+  jogador.pontos = jogador.pontos + 3
+  exibirNaTela();
 }
 function adicionarEmpate(jogador) {
   jogador.empate++
   jogador.pontos++
   //jogador.pontos = jogador.pontos + 1
-  exibirNaTela()
+  exibirNaTela();
 }
 
-function adicionarDerrota(jogador) {}
+function adicionarDerrota(jogador) {
+  jogador.derrota++;
+  exibirNaTela();
+}
 
-
+//Continuar amanhã nos 34 minutos do curso
